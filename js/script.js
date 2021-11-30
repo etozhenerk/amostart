d7ac80b53bbd8fd20c38c0c574221547c739ca2d
mmalahov <mmalahov@pop-os.localdomain>
2021-11-30 18:11:17 +0300

    5

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   loop: false,
   initialSlide: 1,
   spaceBetween: 10,
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
   centeredSlides: true,
   centeredSlidesBounds: true,
   // Navigation arrows
   navigation: {
-    nextEl: '.slider__button-next',
-    prevEl: '.slider__button-prev',
+    nextEl: ".slider__button-next",
+    prevEl: ".slider__button-prev",
   },
   breakpoints: {
     992: {
-      spaceBetween: 60
-    }
-  }
+      spaceBetween: 60,
+    },
+  },
 });
 
 const accordeon = () => {
@@ -46,24 +46,57 @@ const accordeon = () => {
 const openModal = () => {
   const overlay = document.querySelector(".overlay");
   const body = document.querySelector("body");
+  const modal = document.querySelector(".modal");
+  const modalForm = document.querySelector(".modal-form");
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-  const open = () => {
+  const openOverlay = () => {
     overlay.classList.add("overlay--isOpen");
-  }
-  const close = () => {
+  };
+  const openModal = () => {
+    modal.classList.add("modal--isOpen");
+  };
+  const openModalForm = () => {
+    modalForm.classList.add("modal-form--isOpen");
+  };
+  const openMobileMenu = () => {
+    mobileMenu.classList.add("mobile-menu--isOpen");
+  };
+  const closeMobileMenu = () => {
+    mobileMenu.classList.remove("mobile-menu--isOpen");
+  };
+  const closeOverlay = () => {
     overlay.classList.remove("overlay--isOpen");
-  }
+  };
+  const closeModal = () => {
+    modal.classList.remove("modal--isOpen");
+  };
+  const closeModalForm = () => {
+    modalForm.classList.remove("modal-form--isOpen");
+  };
 
-  body.addEventListener('click', e => {
+  body.addEventListener("click", (e) => {
     const target = e.target;
-    if(target.closest(".card")){
-      open();
+    if (target.closest(".card")) {
+      openOverlay();
+      openModal();
     }
-    if(target.closest(".modal__close") || target === overlay){
-      close();
+    if (target.closest(".header__button")) {
+      openOverlay();
+      openModalForm();
     }
-  })
-}
+    if (target.closest(".header__menu")) {
+      openOverlay();
+      openMobileMenu();
+    }
+    if (target.closest(".modal__close") || target === overlay) {
+      closeOverlay();
+      closeModal();
+      closeModalForm();
+      closeMobileMenu();
+    }
+  });
+};
 
 accordeon();
 openModal();
